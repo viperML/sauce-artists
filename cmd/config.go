f6be4f29cc989de51b3be15b55969c8cde7156da
Fernando Ayats <ayatsfer@gmail.com>
2024-10-23 21:51:49 +0200

    query saucenao

---
+++ cmd/config.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	username string
+	api_key  string
+	favgroup string
+}
+
+func GetConfig() Config {
+	username, found := os.LookupEnv("DANBOORU_USERNAME")
+	if !found {
+		log.Fatal("DANBOORU_USERNAME not set")
+	}
+
+	api_key, found := os.LookupEnv("DANBOORU_APIKEY")
+	if !found {
+		log.Fatal("DANBOORU_APIKEY not set")
+	}
+
+	favgroup, found := os.LookupEnv("DANBOORU_FAVGROUP")
+	if !found {
+		log.Fatal("DANBOORU_FAVGROUP not set")
+	}
+
+	res := Config{
+		username: username,
+		api_key:  api_key,
+		favgroup: favgroup,
+	}
+
+	return res
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,63 @@
 package cmd
 
 import (
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
-func Execute() {
+type FavResp struct {
+	id         int32
+	name       string
+	creator_id int32
+	// post_ids   []int32
+}
 
-    resp, err := http.Get("http://example.com/")
+func Execute() {
+	config := GetConfig()
+
+	url, err := url.Parse("https://danbooru.donmai.us/favorite_groups/" + fmt.Sprint(config.favgroup) + ".json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	values := url.Query()
+	values.Add("login", config.username)
+	values.Add("api_key", config.api_key)
+	url.RawQuery = values.Encode()
+	log.Print(url.String())
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("User-Agent", "curl/8.9.1")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(resp)
+
+	ct := strings.Split(resp.Header.Get("Content-Type"), ";")[0]
+	if ct != "application/json" {
+		log.Fatalf("Content-Type was <%s>", ct)
+	}
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body := string(bytes)
+	log.Print(body)
+
+	var favresp map[string](any)
+    err = json.Unmarshal(bytes, &favresp)
     if err != nil {
-        fmt.Printf("%s", err)
-    } else {
-        fmt.Printf("%v", resp)
+        log.Fatal(err)
     }
+    log.Printf("%+v", favresp)
 }
