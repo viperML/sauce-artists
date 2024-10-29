6d2266746bb412cb38ade5b0719616215e675725
Fernando Ayats <ayatsfer@gmail.com>
2024-10-29 22:04:29 +0100

    test send

--- bot/main.go
+++ bot/main.go
@@ -3,48 +3,47 @@ package bot
 import (
 	"log"
 	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/bwmarrin/discordgo"
 )
 
-
 func Execute() {
-    token, found := os.LookupEnv("DISCORD_BOT_TOKEN")
-    if !found {
-        log.Fatal("DISCORD_BOT_TOKEN is not set")
-    }
+	token, found := os.LookupEnv("DISCORD_BOT_TOKEN")
+	if !found {
+		log.Fatal("DISCORD_BOT_TOKEN is not set")
+	}
 
-    dg, err := discordgo.New("Bot " + token)
-    if err != nil {
-        log.Fatal(err)
-    }
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    dg.AddHandler(handler)
+	// dg.AddHandler(handler)
 
-    dg.Identify.Intents = discordgo.IntentGuildMessages
+	dg.Identify.Intents = discordgo.IntentGuildMessages
 
-    err = dg.Open()
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = dg.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    log.Print("Bot running")
-    sc := make(chan os.Signal, 1)
-    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-    <-sc
+	channel, found := os.LookupEnv("DISCORD_CHANNEL_ID")
+	if !found {
+		log.Fatal("DISCORD_CHANNEL_ID not set")
+	}
 
-    dg.Close()
+    dg.ChannelMessageSend(channel, "https://danbooru.donmai.us/posts")
+
+	// log.Print("Bot running")
+	// sc := make(chan os.Signal, 1)
+	// signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	// <-sc
+
+	dg.Close()
 }
 
-func handler(s *discordgo.Session, m *discordgo.MessageCreate) {
-    log.Print(m)
-
-    channel, found := os.LookupEnv("DISCORD_CHANNEL_ID")
-    if !found {
-        log.Fatal("DISCORD_CHANNEL_ID not set")
-    }
-
-    s.ChannelMessageSend(channel, "ping")
-}
+// func handler(s *discordgo.Session, m *discordgo.MessageCreate) {
+//     log.Print(m)
+//
+//
+//     s.ChannelMessageSend(channel, "ping")
+// }
